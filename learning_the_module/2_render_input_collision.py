a49b03ed1aa394b94b65c19f07917bb972f693db
wafforyu <wafforyu@gmail.com>
2021-03-27 12:08:45 +0800

    to add collision

--- learning_the_module/2_render_input_collision.py
+++ learning_the_module/2_render_input_collision.py
@@ -1,19 +1,62 @@
-import pygame, sys
+import pygame, sys #imports
 from pygame.locals import *
-pygame.init()
+pygame.init() #initialize pygame
 
-clock_fps = pygame.time.Clock()
-WINDOW_SIZE = (400,400)
-WIN = pygame.display.set_mode(WINDOW_SIZE,0,32)
-pygame.display.set_caption("Rendering")
+clock_fps = pygame.time.Clock() #create clock speed / fps
+WINDOW_SIZE = (800,600) #set window size
+WIN = pygame.display.set_mode(WINDOW_SIZE,0,32) #create window
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+pygame.display.set_caption("Rendering") #window title/caption
 
-    pygame.display.update()
-    clock_fps.tick(60)
+player_img = pygame.image.load("learning_the_module/Assets/Lasthe.png") #import image
+
+player_position = [50,50] #player location tracker
+
+player_speed = 10
+
+moving_left = False #initial player movement
+moving_right = False
+
+player_y_momentum = 0
+player_x_momentum = 0
+
+while True: #game loop
+
+    WIN.fill((0,0,0)) #fills the screen with black again just before a new event is triggered
+
+    WIN.blit(player_img,(player_position)) #render image in screen (x16,y16) note: y is inverted
+
+    #if player is touching the y limit then bounce up
+    if player_position[1] > WINDOW_SIZE[1] - player_img.get_height(): #[1] refers to the y axis
+        player_y_momentum = -player_y_momentum #-y axis means to go up// BOUNCE
+    else:
+        player_y_momentum += 0.2 #gravity, this will pull the player down to the y axis at 0.2 rate
+
+    player_position[1] += player_y_momentum #players y will constantlt update based on the y momentum changes
+
+    if moving_right:
+        player_position[0] += player_speed #0 stands for x axis
+    if moving_left:
+        player_position[0] -= player_speed #0 stands for x axis
+
+    for event in pygame.event.get(): #event listener
+        if event.type == QUIT: #close button on window
+            pygame.quit() #stop pygame
+            sys.exit() #stop script
+
+        if event.type == KEYDOWN: #on key press
+            if event.key == K_RIGHT: #RIGHT ARROW KEY
+                moving_right = True
+            if event.key == K_LEFT: #LEFT ARROW KEY
+                moving_left = True
+
+        if event.type == KEYUP: #on key release
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
+
+    pygame.display.update() #updates the screen display
+    clock_fps.tick(60) #sets the fps
 
     
